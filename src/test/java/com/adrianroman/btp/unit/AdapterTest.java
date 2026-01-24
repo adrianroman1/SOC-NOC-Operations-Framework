21615578bb7742383556e8b149db68ea7ff54102
adrianroman1 <88735805+adrianroman1@users.noreply.github.com>
2026-01-24 12:07:07 +0200

    Create AdapterTest.java

---
+++ src/test/java/com/adrianroman/btp/unit/AdapterTest.java
@@ -0,0 +1,14 @@
+package com.adrianroman.btp.unit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class AdapterTest {
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+}
+
